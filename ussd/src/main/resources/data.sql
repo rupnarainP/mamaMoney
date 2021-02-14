108c3acf2c5f593eba45e2c13c10b0197fe92c46
DAX-PC\DaX <prdaxter@gmail.com>
2021-02-14 13:48:42 +0200

    Added more rows to the database.

--- ussd/src/main/resources/data.sql
+++ ussd/src/main/resources/data.sql
@@ -1,1 +1,3 @@
 insert into Ussd values('#AA1234', null, '27821231234', 0, null);
+insert into Ussd values('#AA1235', null, '27821231235', 0, null);
+insert into Ussd values('#AA1236', null, '27821231236', 0, null);
